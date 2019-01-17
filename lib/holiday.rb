4c033a470e37469ce66af306868476e69c4c30e6
Patricia Padilha Coleman <patricia.p.coleman@gmail.com>
2019-01-17 19:31:11 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -95,7 +95,17 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+  
+  holiday_hash.each do |season, holiday|
+   
+    holiday.each do |name, list|
+      
+      if list.include
+      
+    end
+        
+  end 
+   
 end
 
 
